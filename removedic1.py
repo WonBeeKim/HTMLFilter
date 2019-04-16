25cd0ec0361e6c05b67ef3fc7863305292d9cfb5
WonBeeKim <43817203+WonBeeKim@users.noreply.github.com>
2019-04-16 10:46:58 +0900

    Update removedic1.py

--- removedic1.py
+++ removedic1.py
@@ -1,19 +1,25 @@
 import re
 
 #1차 삭제파일의 경로
-file = open('C:/Users/wonbee/1차삭제.txt',"r", encoding = "utf-8")
+#Path to 1stdic file
+file = open('C:/Users/wonbee/1stdic.txt',"r", encoding = "utf-8")
 
 # 텍스트 추출
+#Text extraction
 html = file.read()
 
 # 소문자변환
+#convert to lowercase
 body=html.lower()
 
-# \s+ 공백과 매칭
+# \s+ : 공백과 매칭
+# \s+ : matching with space
 pattern = re.compile('\s+')
 
 #|를 입력하는 이유:단어를 나누는 경계임.
 #단어 사이에 줄바꿈 대신 | 삽입
+#Why enter | : The boundary between words.
+#Insert | instead of \n between words
 result = re.sub(pattern, '|', body)
 
 print(result)
